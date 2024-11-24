f1a2108c1d833d77d1a52a5aab77baec89935452
Jose Negrin <soyjosenegrin@gmail.com>
2024-11-24 22:55:33 +0100

    refactor: change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client"
-import { SearchBar } from "@/components/SearchBar/SearchBar";
+import { SearchBar } from "@/components/SearchBar";
 import styles from "./page.module.css";
-import { Logo } from "@/components/Logo/Logo";
+import { Logo } from "@/components/Logo";
+import { SearchButton } from "@/components/SearchButton";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main_container}>
-        <Logo />
-        <SearchBar large/>
-      </main>
-    </div>
+    <section className={styles.main_container}>
+      <Logo large />
+      <SearchBar isLarge />
+      <SearchButton />
+    </section>
   );
 }
